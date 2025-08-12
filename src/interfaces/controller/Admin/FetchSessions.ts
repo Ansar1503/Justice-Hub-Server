52db31115f1324032acdca377a6d0e3daf44c899
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-12 21:02:23 +0530

    fixed - FetchSession/Admin

--- src/interfaces/controller/Admin/FetchSessions.ts
+++ src/interfaces/controller/Admin/FetchSessions.ts
@@ -18,23 +18,24 @@ export class FetchSessions implements IController {
   async handle(httpRequest: HttpRequest): Promise<IHttpResponse> {
     const parsedData = zodSessionQuerySchema.safeParse(httpRequest.query);
     if (!parsedData.success) {
-      // console.log("parsed Error :", parsedData.error);
-      parsedData.error.errors.forEach((err) => {
-        return new HttpResponse(Number(err.code), err.message);
-      });
+      console.log("parsed Error :", parsedData.error);
+      const err = parsedData.error.errors[0];
+      return this.httpErrors.error_400(err.message);
     }
     try {
       if (!parsedData.data) {
         return this.httpErrors.error_400("Invalid Credentials");
       }
       const result = await this.fetchSessionUseCase.execute(parsedData.data);
-      console.log("result", result);
+      // console.log("result", result);
       const success = this.httpSuccess.success_200(result);
       return new HttpResponse(success.statusCode, success.body);
     } catch (error) {
-      console.log("error:", error);
-      const err = this.httpErrors.error_500();
-      return new HttpResponse(err.statusCode, err.body);
+      // console.log("error:", error);
+      if (error instanceof Error) {
+        return this.httpErrors.error_400(error.message);
+      }
+      return this.httpErrors.error_500();
     }
   }
 }
