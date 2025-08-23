4086d982d2da107748399aaeb69de278e7917960
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-23 08:50:09 +0530

    deleted un used usecases dtos and composers

--- src/application/dtos/AppointmentsDtos.ts
+++
@@ -1,9 +0,0 @@
-type appointmentstatustype =
-  | "all"
-  | "confirmed"
-  | "pending"
-  | "completed"
-  | "cancelled"
-  | "rejected";
-
-type sortFieldType = "name" | "consultation_fee" | "date" | "created_at";
--- src/application/usecases/Lawyer/IFetchAppointmentDetailsLawyerUseCase.ts
+++
@@ -1,8 +0,0 @@
-import {
-  FetchAppointmentInputDto,
-  FetchAppointmentsOutputDto,
-} from "@src/application/dtos/Lawyer/FetchAppointmentDto";
-import { IUseCase } from "../IUseCases/IUseCase";
-
-export interface IFetchAppointmentDetailsLawyerUseCase
-  extends IUseCase<FetchAppointmentInputDto, FetchAppointmentsOutputDto> {}
