fa77e5013d36879beadd42aab14bc634e94b1271
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-27 21:06:36 +0530

    wallet model created

---
+++ src/infrastructure/database/model/WalletModel.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IWalletModel extends Document {
+  _id: string;
+  user_id: string;
+  balance: number;
+  status: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const walletSchema = new Schema<IWalletModel>({
+  _id: { type: String },
+  user_id: { type: String, required: true },
+  balance: { type: Number, required: true, min: 0, max: 100000 },
+  status: { type: Boolean, default: true },
+  createdAt: { type: Date, required: true },
+  updatedAt: { type: Date, required: true },
+});
+
+export const WalletModel = mongoose.model<IWalletModel>(
+  "wallets",
+  walletSchema
+);
