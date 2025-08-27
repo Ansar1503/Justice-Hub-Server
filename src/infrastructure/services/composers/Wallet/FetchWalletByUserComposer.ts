c637d7024f977e1cdb0fce2b4d0a9970a7b6d29b
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-27 23:43:50 +0530

    fetch wallet by user componser

---
+++ src/infrastructure/services/composers/Wallet/FetchWalletByUserComposer.ts
@@ -0,0 +1,9 @@
+import { WalletRepo } from "@infrastructure/database/repo/WalletRepo";
+import { IController } from "@interfaces/controller/Interface/IController";
+import { FetchWalletByUserController } from "@interfaces/controller/wallet/FetchWalletByUser";
+import { FetchWalletByUser } from "@src/application/usecases/Wallet/implementation/FetchWalletByUser";
+
+export function FetchWalletByUserComposer(): IController {
+  const usecase = new FetchWalletByUser(new WalletRepo());
+  return new FetchWalletByUserController(usecase);
+}
