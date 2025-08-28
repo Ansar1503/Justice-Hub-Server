5dc483dc2c64923b56e96dd478c75fff57a5988a
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-28 17:35:40 +0530

    feat: add WalletTransactionDto and initial structure for AddFundsUsecase

---
+++ src/application/dtos/wallet/WalletTransactionDto.ts
@@ -0,0 +1,27 @@
+type transactionCategory =
+  | "deposit"
+  | "withdrawal"
+  | "payment"
+  | "refund"
+  | "transfer";
+type transactionStatus = "pending" | "completed" | "failed" | "cancelled";
+type transactionType = "debit" | "credit";
+
+export interface WalletTransactionDto {
+  id: string;
+  walletId: string;
+  amount: number;
+  type: transactionType;
+  description: string;
+  category: transactionCategory;
+  status: transactionStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// export interface addFundsToWalletInputDto {
+//   userId: string;
+//   amount: number;
+// }
+
+// export interface addFundsToWalletOutputDto {}
---
+++ src/application/usecases/Wallet/IAddFundsUsecase.ts
@@ -0,0 +1,9 @@
+// import { WalletDto } from "@src/application/dtos/wallet/WalletDto";
+// import { IUseCase } from "../IUseCases/IUseCase";
+// import {
+//   addFundsToWalletInputDto,
+//   addFundsToWalletOutputDto,
+// } from "@src/application/dtos/wallet/WalletTransactionDto";
+
+// export interface IAddFundsUsecase
+//   extends IUseCase<addFundsToWalletInputDto, addFundsToWalletOutputDto> {}
