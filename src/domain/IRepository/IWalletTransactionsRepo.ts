4d7825819ebaa4ec9621a5a670a4f1a082ba4f0c
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-28 18:38:02 +0530

    feat: implement findTransactionsByWalletId method in WalletTransactionsRepo for fetching wallet transactions by wallet ID

--- src/domain/IRepository/IWalletTransactionsRepo.ts
+++ src/domain/IRepository/IWalletTransactionsRepo.ts
@@ -2,4 +2,13 @@ import { WalletTransaction } from "@domain/entities/WalletTransactions";
 import { IBaseRepository } from "./IBaseRepo";
 
 export interface IWalletTransactionsRepo
-  extends IBaseRepository<WalletTransaction> {}
+  extends IBaseRepository<WalletTransaction> {
+  findTransactionsByWalletId(payload: {
+    walletId: string;
+    page: number;
+  }): Promise<{
+    data: WalletTransaction[] | [];
+    page: number;
+    totalPages: number;
+  }>;
+}
--- src/infrastructure/database/repo/WalletTransactionsRepo.ts
+++ src/infrastructure/database/repo/WalletTransactionsRepo.ts
@@ -14,4 +14,33 @@ export class WalletTransactionsRepo
   constructor() {
     super(walletTransactionModel, new WalletTransactionMapper());
   }
+  async findTransactionsByWalletId({
+    walletId,
+    page,
+  }: {
+    walletId: string;
+    page: number;
+  }): Promise<{
+    data: WalletTransaction[] | [];
+    page: number;
+    totalPages: number;
+  }> {
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const transactions = await this.model
+      .find({ walletId })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+    const total = await this.model.countDocuments({ walletId });
+    return {
+      data:
+        transactions && this.mapper.toDomainArray
+          ? this.mapper.toDomainArray(transactions)
+          : [],
+      page,
+      totalPages: Math.ceil(total / limit),
+    };
+  }
 }
