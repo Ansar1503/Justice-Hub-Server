2e80c7a53f496af2256f370f34cdac3b91bdf15f
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-29 01:00:43 +0530

    feat: update CancelAppointmentComposer to include WalletRepo and WalletTransactionsRepo in use case

--- src/infrastructure/services/composers/Client/Appointment/CancelAppointmentsComposer.ts
+++ src/infrastructure/services/composers/Client/Appointment/CancelAppointmentsComposer.ts
@@ -1,9 +1,17 @@
 import { AppointmentsRepository } from "@infrastructure/database/repo/AppointmentsRepo";
+import { WalletRepo } from "@infrastructure/database/repo/WalletRepo";
+import { WalletTransactionsRepo } from "@infrastructure/database/repo/WalletTransactionsRepo";
+import { MongoUnitofWork } from "@infrastructure/database/UnitofWork/implementations/UnitofWork";
 import { CancelAppointmentController } from "@interfaces/controller/Client/Appointment/CancelAppoitmentController";
 import { IController } from "@interfaces/controller/Interface/IController";
 import { CancelAppointmentUseCase } from "@src/application/usecases/Client/implementations/CancelAppointmentUseCase";
 
 export function CancelAppointmentComposer(): IController {
-  const useCase = new CancelAppointmentUseCase(new AppointmentsRepository());
-  return new CancelAppointmentController(useCase)
+  const useCase = new CancelAppointmentUseCase(
+    new AppointmentsRepository(),
+    new WalletRepo(),
+    new WalletTransactionsRepo(),
+    new MongoUnitofWork()
+  );
+  return new CancelAppointmentController(useCase);
 }
