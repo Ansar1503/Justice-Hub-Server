9bf92b4bc9c87bd6ba583b871c9a202aa00abd27
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-28 12:23:58 +0530

    add wallet repositories to HandleWebhookComposer

--- src/infrastructure/services/composers/Client/StripeWebHookComposer.ts
+++ src/infrastructure/services/composers/Client/StripeWebHookComposer.ts
@@ -3,11 +3,15 @@ import { HandleWebhookController } from "@interfaces/controller/Client/StripeWeb
 import { HandleStripeHookUseCase } from "@src/application/usecases/Client/implementations/HandleStripeHooksUseCase";
 import { ScheduleSettingsRepository } from "@infrastructure/database/repo/ScheduleSettingsRepo";
 import { AppointmentsRepository } from "@infrastructure/database/repo/AppointmentsRepo";
+import { WalletRepo } from "@infrastructure/database/repo/WalletRepo";
+import { WalletTransactionsRepo } from "@infrastructure/database/repo/WalletTransactionsRepo";
 
 export function HandleWebhookComposer(): IController {
   const usecase = new HandleStripeHookUseCase(
     new ScheduleSettingsRepository(),
-    new AppointmentsRepository()
+    new AppointmentsRepository(),
+    new WalletRepo(),
+    new WalletTransactionsRepo()
   );
   return new HandleWebhookController(usecase);
 }
