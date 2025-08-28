394a7b7fda430706d14c4d2491f43ce7307e8557
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-29 00:56:06 +0530

    feat: add optional ClientSession parameter to BaseRepository constructor and update create method to use session

--- src/infrastructure/database/repo/base/BaseRepo.ts
+++ src/infrastructure/database/repo/base/BaseRepo.ts
@@ -1,20 +1,22 @@
 import { IBaseRepository } from "@domain/IRepository/IBaseRepo";
 import { IMapper } from "@infrastructure/Mapper/IMapper";
-import { Model } from "mongoose";
+import { ClientSession, Model } from "mongoose";
 
 export abstract class BaseRepository<TDomain, TPersistence>
   implements IBaseRepository<TDomain>
 {
   constructor(
     protected readonly model: Model<TPersistence>,
-    protected readonly mapper: IMapper<TDomain, TPersistence>
+    protected readonly mapper: IMapper<TDomain, TPersistence>,
+    protected readonly session?: ClientSession
   ) {}
 
   async create(entity: TDomain): Promise<TDomain> {
     const persistence = this.mapper.toPersistence(entity);
-    const saved = await this.model.create(persistence).catch((err) => {
+    const doc = new this.model(persistence);
+    const saved = await doc.save({ session: this.session }).catch((err) => {
       throw new Error(err.message);
     });
-    return this.mapper.toDomain(saved);
+    return this.mapper.toDomain(saved.toObject());
   }
 }
