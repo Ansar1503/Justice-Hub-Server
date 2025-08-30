bcb52ab3294d6436100522360bde8f1be1a90fb1
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-30 16:30:40 +0530

    practice area model

---
+++ src/infrastructure/database/model/PracticeAreaModel.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IPracticeareaModel extends Document {
+  _id: string;
+  name: string;
+  specializationId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const practiceareaSchema = new Schema<IPracticeareaModel>(
+  {
+    _id: { type: String },
+    name: { type: String, required: true },
+    specializationId: { type: String, required: true, ref: "specializations" },
+  },
+  { timestamps: true }
+);
+
+export const practiceareaModel = mongoose.model<IPracticeareaModel>(
+  "practiceareas",
+  practiceareaSchema
+);
