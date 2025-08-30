c3534704e9ec54dac6776906f24aeea8b4eda7ca
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-30 16:30:29 +0530

    case type model database model

---
+++ src/infrastructure/database/model/CaseTypeModel.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface ICasetypeModel {
+  _id: string;
+  name: string;
+  practiceareaId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const casettypeSchema = new Schema<ICasetypeModel>(
+  {
+    _id: { type: String },
+    name: { type: String, required: true },
+    practiceareaId: { type: String, required: true, ref: "practiceareas" },
+  },
+  { timestamps: true }
+);
+
+export const CasetypeModel = mongoose.model<ICasetypeModel>(
+  "casetypes",
+  casettypeSchema
+);
