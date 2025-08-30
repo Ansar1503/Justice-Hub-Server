2f73e0f2c2900d252af69f1af99bc54776a3bc48
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-30 16:30:58 +0530

    specialisations model

---
+++ src/infrastructure/database/model/SpecializationModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface ISpecializationModel extends Document {
+  _id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const specializationSchema = new Schema<ISpecializationModel>(
+  {
+    _id: { type: String },
+    name: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const SpecializationModel = mongoose.model<ISpecializationModel>(
+  "specializations",
+  specializationSchema
+);
