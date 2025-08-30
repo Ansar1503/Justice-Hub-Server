56a35b2e38f97e89d2f98cd9326c4b0295f0471d
Ansar1503 <muhammedansarma1503@gmail.com>
2025-08-30 15:41:18 +0530

    specialisation repo interface

---
+++ src/domain/IRepository/ISpecialization.ts
@@ -0,0 +1,4 @@
+import { Specialization } from "@domain/entities/Specialization";
+import { IBaseRepository } from "./IBaseRepo";
+
+export interface ISpecializationRepo extends IBaseRepository<Specialization> {}
