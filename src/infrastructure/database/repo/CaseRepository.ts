3e715573c53146470e12c890adc8bcacadcbe015
Ansar1503 <muhammedansarma1503@gmail.com>
2025-10-09 10:51:43 +0530

    update repo

--- src/infrastructure/database/repo/CaseRepository.ts
+++ src/infrastructure/database/repo/CaseRepository.ts
@@ -354,7 +354,7 @@ export class CaseRepository
 
   async findAllByUser(userId: string): Promise<Case[] | []> {
     const data = await this.model.find({
-      $or: [{ clientId: userId, lawyerId: userId }],
+      $or: [{ clientId: userId }, { lawyerId: userId }],
     });
     return data && this.mapper.toDomainArray
       ? this.mapper.toDomainArray(data)
