5a3ffbdf614a4784ad48fe150b1c2494d91b1e08
Ansar1503 <muhammedansarma1503@gmail.com>
2025-12-30 09:38:45 +0530

    feat: Add notification creation use case and Mongoose model for persistent storage.

--- src/infrastructure/database/model/NotificationModel.ts
+++ src/infrastructure/database/model/NotificationModel.ts
@@ -3,32 +3,35 @@ import mongoose, { Document, Schema } from "mongoose";
 type NotificationType = "message" | "session";
 
 export interface INotificationModel extends Document {
-    _id: string;
-    recipientId: string;
-    senderId: string;
-    type: NotificationType;
-    roomId: string;
-    sessionId: string;
-    title: string;
-    message: string;
-    isRead: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: string;
+  recipientId: string;
+  senderId: string;
+  type: NotificationType;
+  roomId: string;
+  sessionId: string;
+  title: string;
+  message: string;
+  isRead: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const NotificationSchema = new Schema<INotificationModel>(
-    {
-        _id: { type: String },
-        isRead: { type: Boolean, default: false },
-        message: { type: String, required: true },
-        recipientId: { type: String, required: true },
-        senderId: { type: String, required: true },
-        title: { type: String, required: true },
-        type: { type: String, enum: ["message", "session"], required: true },
-        roomId: { type: String, required: false },
-        sessionId: { type: String, required: false },
-    },
-    { timestamps: true },
+  {
+    _id: { type: String },
+    isRead: { type: Boolean, default: false },
+    message: { type: String },
+    recipientId: { type: String, required: true },
+    senderId: { type: String, required: true },
+    title: { type: String, required: true },
+    type: { type: String, enum: ["message", "session"], required: true },
+    roomId: { type: String, required: false },
+    sessionId: { type: String, required: false },
+  },
+  { timestamps: true }
 );
 
-export const NotificationModel = mongoose.model<INotificationModel>("Notification", NotificationSchema);
+export const NotificationModel = mongoose.model<INotificationModel>(
+  "Notification",
+  NotificationSchema
+);
