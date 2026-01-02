e3b6df1e3cb73d17f80349b7000fcc56d86198b2
Ansar1503 <muhammedansarma1503@gmail.com>
2026-01-03 01:35:03 +0530

    feat: Add GetMessagesComposer for chat message retrieval.

--- src/infrastructure/services/composers/Chats/GetMessagesComposer.ts
+++ src/infrastructure/services/composers/Chats/GetMessagesComposer.ts
@@ -2,16 +2,19 @@ import { ChatMessageRepository } from "@infrastructure/database/repo/ChatMessage
 import { ChatSessionRepository } from "@infrastructure/database/repo/ChatSessionRepo";
 import { DisputesRepo } from "@infrastructure/database/repo/DisputesRepo";
 import { SessionsRepository } from "@infrastructure/database/repo/SessionRepo";
+import { UserSubscriptionRepository } from "@infrastructure/database/repo/UserSubscriptionRepository";
+import { UserSubscriptionMapper } from "@infrastructure/Mapper/Implementations/UserSubscriptionMapper";
 import { GetMessages } from "@interfaces/controller/Chats/GetMessages";
 import { IController } from "@interfaces/controller/Interface/IController";
 import { ChatUseCase } from "@src/application/usecases/chat.usecase";
 
 export function GetMessagesComposer(): IController {
-    const usecase = new ChatUseCase(
-        new ChatSessionRepository(),
-        new ChatMessageRepository(),
-        new SessionsRepository(),
-        new DisputesRepo(),
-    );
-    return new GetMessages(usecase);
+  const usecase = new ChatUseCase(
+    new ChatSessionRepository(),
+    new ChatMessageRepository(),
+    new SessionsRepository(),
+    new DisputesRepo(),
+    new UserSubscriptionRepository(new UserSubscriptionMapper())
+  );
+  return new GetMessages(usecase);
 }
