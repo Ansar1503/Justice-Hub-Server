2d5953d8b8aebd9d4b91092c967e3641fa244dd9
Ansar1503 <muhammedansarma1503@gmail.com>
2026-01-03 01:38:32 +0530

    feat: Add FetchSessionsController for lawyer session retrieval with filtering and pagination.

--- src/application/usecases/Lawyer/IFetchSessionDocumentsUseCase.ts
+++
@@ -1,4 +0,0 @@
-import { FindSessionDocumentOutputDto } from "@src/application/dtos/Lawyer/FindSessionDocumentDto";
-import { IUseCase } from "../IUseCases/IUseCase";
-
-export interface IFetchSessionDocumentsUseCase extends IUseCase<string, FindSessionDocumentOutputDto> {}
--- src/infrastructure/services/composers/Lawyer/Session/FetchSessionDocumentsComposer.ts
+++ src/infrastructure/services/composers/Lawyer/Session/FetchSessionDocumentsComposer.ts
@@ -1,9 +1,9 @@
-import { IController } from "@interfaces/controller/Interface/IController";
-import { FetchSessionsDocumentsController } from "@interfaces/controller/Lawyer/Sessions/FetchSessionDocuments";
-import { FetchSessionsDocumentsUseCase } from "@src/application/usecases/Lawyer/implementations/FetchSessionDocumentsUseCase";
-import { SessionDocumentsRepository } from "@infrastructure/database/repo/SessionsDocumentRepo";
+// import { IController } from "@interfaces/controller/Interface/IController";
+// import { FetchSessionsDocumentsController } from "@interfaces/controller/Lawyer/Sessions/FetchSessionDocuments";
+// import { FetchSessionsDocumentsUseCase } from "@src/application/usecases/Lawyer/implementations/FetchSessionDocumentsUseCase";
+// import { SessionDocumentsRepository } from "@infrastructure/database/repo/SessionsDocumentRepo";
 
-export function FetchSessionDocumentsComposer(): IController {
-    const usecase = new FetchSessionsDocumentsUseCase(new SessionDocumentsRepository());
-    return new FetchSessionsDocumentsController(usecase);
-}
+// export function FetchSessionDocumentsComposer(): IController {
+//     const usecase = new FetchSessionsDocumentsUseCase(new SessionDocumentsRepository());
+//     return new FetchSessionsDocumentsController(usecase);
+// }
